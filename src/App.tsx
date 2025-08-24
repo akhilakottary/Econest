86cf49c49ab77d7580add10f9501ce5b1de1d436
akhilakottary <git config --global user.email akhilakottary26@gmail.com>
2025-08-24 19:53:25 +0530

    feat: add dashboard with search and plant cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/user/login";
-// import Dashboard from "./pages/user/dashboard";
+import Dashboard from "./pages/user/dashboard";
 // import CartPage from "./pages/user/cartpage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/cartpage" element={<CartPage />} /> */}
+      <Route path="/dashboard" element={<Dashboard />} />
+      {/* <Route path="/cartpage" element={<CartPage />} /> */}
     </Routes>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 // src/main.tsx
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { Provider } from 'react-redux'
 // import { store } from './app/store'
